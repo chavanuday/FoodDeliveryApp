3e9dcb07ec3bd5351a9d79294117bff460687759
Uday Chavan <udaymchavan228@gmail.com>
2024-02-04 05:05:58 +0530

    React HOOKS added in body, on listOfRestaurant

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,42 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mokData";
+import { useState } from "react";
+
 
 const Body = () => {
-    return (
-      <div className="body">
-        <div className="search-container">Search</div>
-        <div className="restaurant-container">
-          {restaurantList.map((restaurant) => (
-            <RestaurantCard
+
+
+  // Local state varable - suuper powerful variable
+
+  const [listOfRestaurant , SetListOfRestaurant ] = useState(restaurantList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            // filter out
+
+          const filteredList = listOfRestaurant.filter(
+              (res) => res.data.avgRating > 4
+            );
+            SetListOfRestaurant(filteredList)
+          }}
+        >
+          Top rated restaurant
+        </button>
+      </div>
+      <div className="restaurant-container">
+        { listOfRestaurant.map((restaurant) => (
+          <RestaurantCard
             key={restaurant.data.id}
             restaurantData={restaurant}
-            />
-          ))}
-        </div>
+          />
+        ))}
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
+
 export default Body;
